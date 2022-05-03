841c499135d7322efaea231a4fd10b0c89afff0e
CihatGUVEN <89444171+CihatGUVEN@users.noreply.github.com>
2022-05-03 12:45:30 +0300

    first commit

---
+++ src/test/java/com/vole/voletwitterclone/VoleTwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vole.voletwitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoleTwitterCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
